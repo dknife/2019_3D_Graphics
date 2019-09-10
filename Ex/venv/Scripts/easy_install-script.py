5aa6f1ba8d00e6ad5e4cf671b0ec95dff8f299cb
Dknife <young.min.kang@gmail.com>
2019-09-10 10:53:40 +0900

    new

---
+++ Ex/venv/Scripts/easy_install-script.py
@@ -0,0 +1,12 @@
+#!C:\작업공간\교육과_교과서_저술\강의\2019\201902_그래픽스\2019_3D_Graphics\Ex\venv\Scripts\python.exe -x
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install'
+__requires__ = 'setuptools==40.8.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('setuptools==40.8.0', 'console_scripts', 'easy_install')()
+    )
---
+++ Ex/venv/Scripts/pip-script.py
@@ -0,0 +1,12 @@
+#!C:\작업공간\교육과_교과서_저술\강의\2019\201902_그래픽스\2019_3D_Graphics\Ex\venv\Scripts\python.exe -x
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip'
+__requires__ = 'pip==19.0.3'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('pip==19.0.3', 'console_scripts', 'pip')()
+    )
